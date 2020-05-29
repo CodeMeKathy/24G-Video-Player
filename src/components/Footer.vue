81932e1157cd5febd4bbd041eee19251882b308c
Kathy * <Kathy@KathyLambert.me>
2020-05-29 01:31:21 -0400

    Feature(Footer): Add new Footer component with styling to App component

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="footer">
+    <img
+      src="../../src/assets/powered_by_24g.svg"
+      alt="Powered by 24G Logo"
+      class="footer-logo"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped lang="scss">
+.footer {
+  display: flex;
+  background-color: #fff;
+  height: 6rem;
+  place-content: center;
+  &-logo {
+    height: 1.3rem;
+    align-self: center;
+  }
+}
+</style>
