b7a3c7148269bce91ae1c6edc2aa3be014b5a9c1
CodeMeKathy * <Kathy@KathyLambert.me>
2020-05-30 17:59:13 -0400

    Feature(Server): Add Server database video schema
    Add database video schema and installed server express, cors, mongoose dev dependencies

---
+++ server/db/models/video.js
@@ -0,0 +1,22 @@
+// Video Schema
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const videoSchema = new Schema({
+  title: { type: String, required: true },
+  thumb: { type: String, required: true },
+  src: { type: String },
+  comments: [
+    { userName: String, userIcon: String, body: String, dateCreated: Date }
+  ],
+  meta: {
+    views: Number,
+    likes: Number,
+    dislikes: Number
+  }
+})
+
+const Video = mongoose.model('Video', videoSchema)
+
+module.exports = Video
